d67a57207be99bf6c5470e5644b8f8c864f935c8
matthew <matthew.almeida@sbcglobal.net>
2023-06-25 22:03:13 -0400

    seeder data

--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -3,41 +3,129 @@ from sqlalchemy.sql import text
 
 def seed_posts():
 
-    user = User.query.get(1)
+    user1 = User.query.get(1)
+    user2 = User.query.get(2)
+    user3 = User.query.get(3)
+    user4 = User.query.get(4)
+    user5 = User.query.get(5)
+    user6 = User.query.get(6)
+    user7 = User.query.get(7)
+    user8 = User.query.get(8)
+    user9 = User.query.get(9)
+    user10 = User.query.get(10)
+    user11 = User.query.get(11)
+    user12 = User.query.get(12)
+
+
 
     post1 = Post(
         userId=1,
         caption="Just finished watching the latest episode of My Hero Academia! The action scenes were mind-blowing. Can't wait for the next one! #anime #MyHeroAcademia",
         video="https://capstone-tiktok.s3.amazonaws.com/Video_4_dxlpb0.mp4",
-        post_favorites = [user],
-        post_likes = [user]
+        post_favorites = [user1, user3, user6, user8],
+        post_likes = [user1, user2, user3]
     )
     post2 = Post(
-        userId=1,
+        userId=2,
         caption="Throwback to one of my all-time favorite anime series, Attack on Titan! The storyline and character development are simply incredible. #anime #AttackOnTitan",
         video="https://capstone-tiktok.s3.amazonaws.com/Video_3_iegkos.mp4",
-        post_favorites = [user]
+        post_favorites = [user1],
+        post_likes = [user3, user8, user9]
+
     )
     post3 = Post(
-        userId=2,
+        userId=3,
         caption="Just started watching Demon Slayer, and I'm already hooked! The animation and fight sequences are stunning. Highly recommend it! #anime #DemonSlayer",
         video="https://capstone-tiktok.s3.amazonaws.com/Video_2_gpq9hh.mp4",
-        post_likes = [user]
+        post_favorites = [user1, user5, user12, user11],
+        post_likes = [user11]
     )
     post4 = Post(
-        userId=3,
+        userId=4,
         caption="Recently discovered One Punch Man, and it's hilarious! Saitama's overpowered character and deadpan humor make it a must-watch. #anime #OnePunchMan",
         video="https://capstone-tiktok.s3.amazonaws.com/Video_1_s9gzxb.mp4",
-        post_likes = [user]
+        post_favorites = [user10, user7, user3, user8],
+        post_likes = [user10]
     )
     post5 = Post(
-        userId=3,
+        userId=5,
         caption="Just binged the entire season of Jujutsu Kaisen, and it blew my mind! The animation, characters, and supernatural elements are top-notch. #anime #JujutsuKaisen",
         video="https://capstone-tiktok.s3.amazonaws.com/Video_ntblmx.mp4",
-        post_favorites = [user]
+        post_favorites = [user3],
+        post_likes = [user1, user2, user3]
+    )
+    post6 = Post(
+        userId=6,
+        caption="When your favorite anime character comes to life...cosplay level: 100! 🎭 #AnimeCosplay #OtakuLife",
+        video="https://capstone-tiktok.s3.amazonaws.com/Video_3.mov",
+        post_favorites = [user1],
+        post_likes = [user5]
+    )
+    post7 = Post(
+        userId=7,
+        caption="That epic anime fight scene got me like...🔥 Can't stop watching! #AnimeAction #BingeWorthy",
+        video="https://capstone-tiktok.s3.amazonaws.com/Video.mov",
+        post_favorites = [user12],
+        post_likes = [user2, user4, user7]
+    )
+    post8 = Post(
+        userId=8,
+        caption="Just a casual day with my anime squad, saving the world one episode at a time! 💪 #AnimeHeroes #Teamwork",
+        video="https://capstone-tiktok.s3.amazonaws.com/Video_1(2).mov",
+        post_favorites = [user1, user6, user7, user9],
+        post_likes = [user2]
+    )
+    post9 = Post(
+        userId=9,
+        caption="When the anime opening hits and you can't resist dancing along! 🎵 #AnimeSoundtrack #WeebLife",
+        video="https://capstone-tiktok.s3.amazonaws.com/Video_1(1).mov",
+        post_favorites = [user1, user6, user3, user10],
+        post_likes = [user11]
+    )
+    post10 = Post(
+        userId=10,
+        caption="Caught in an anime love triangle...who will win my heart? 💔 #AnimeRomance #ShipGoals",
+        video="https://capstone-tiktok.s3.amazonaws.com/Video(2).mov",
+        post_favorites = [user12],
+        post_likes = [user2, user9, user4]
+    )
+    post11 = Post(
+        userId=11,
+        caption="Attempting to recreate that jaw-dropping anime food...did I nail it? 🍜 #AnimeFoodie #CookingChallenge",
+        video="https://capstone-tiktok.s3.amazonaws.com/Video(1).mov",
+        post_favorites = [user4],
+        post_likes = [user8]
+    )
+    post12 = Post(
+        userId=12,
+        caption="Anime marathon night! Snacks? Check. Blanket fort? Check. Let's dive into this epic adventure! 📺 #AnimeMarathon #StayCozy",
+        video="https://capstone-tiktok.s3.amazonaws.com/Video_1.mov",
+        post_favorites = [user12],
+        post_likes = [user10, user11, user5]
+    )
+    post13 = Post(
+        userId=2,
+        caption="Living out my anime dream as a magical girl! Transforming powers activated! ✨ #MagicalGirlVibes #CosplayMagic",
+        video="https://capstone-tiktok.s3.amazonaws.com/Video_3(1).mov",
+        post_favorites = [user1, user5, user2, user7],
+        post_likes = [user6]
+    )
+    post14 = Post(
+        userId=7,
+        caption="Cosplaying as my favorite anime character at a convention! The hype is unreal! 🎉 #AnimeConvention #CosplayCommunity",
+        video="https://capstone-tiktok.s3.amazonaws.com/Video_2(1).mov",
+        post_favorites = [user12, user3, user7, user9],
+        post_likes = [user9]
+    )
+    post15 = Post(
+        userId=1,
+        caption="Sharing my latest anime artwork! Drawing my beloved characters brings me joy. 🎨 #AnimeArt #FanArtist",
+        video="https://capstone-tiktok.s3.amazonaws.com/Video_2.mov",
+        post_favorites = [user6],
+        post_likes = [user4, user3, user9]
     )
 
-    all_posts = [post1, post2, post3, post4, post5 ]
+    all_posts = [post1, post2, post3, post4, post5, post6, post7, post8, post9, post10, post11, post12, post13, post14, post15 ]
     _ = [db.session.add(post) for post in all_posts]
     db.session.commit()
 
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,27 +4,133 @@ from sqlalchemy.sql import text
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    user_1 = User.query.get(1)
-    user_2 = User.query.get(2)
-    user_3 = User.query.get(3)
 
     demo = User(
-        username='Demo', email='demo@aa.io', password='password', bio='testing', profile_pic="https://w0.peakpx.com/wallpaper/638/468/HD-wallpaper-naruto-live-naruto-uzumaki-portrait-manga.jpg")
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password', bio='testing', name="marnie", profile_pic="https://cdn.shopify.com/s/files/1/0046/2779/1960/files/sasuke_uchiha.jpg?v=1584818680")
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password', bio='testing', name="bobbie", profile_pic="https://cdn-0.practicaltyping.com/wp-content/uploads/2019/07/Sakura.png")
+        username='Demo', email='demo@aa.io', password='password', bio='Here for the Anime!', name="DemoUser", profile_pic="https://static1.squarespace.com/static/5c8a7b5cca525b3868731c5d/t/5efaaf94ba3a2c05d16cc6d5/1593487252811/NGC_avatars-02.png")
+    sasuke = User(
+        username='SasukeUchiha', email='sasuke@aa.io', password='password', bio='Vengeful avenger seeking redemption and power.', name="Sasuke", profile_pic="https://cdn.shopify.com/s/files/1/0046/2779/1960/files/sasuke_uchiha.jpg?v=1584818680")
+    sakura  = User(
+        username='SakuraHaruno ', email='sakura@aa.io', password='password', bio='Skilled kunoichi with a blossoming inner strength.', name="Sakura", profile_pic="https://cdn-0.practicaltyping.com/wp-content/uploads/2019/07/Sakura.png")
+    monkey  = User(
+        username='MonkeyLuffy', email='monkey@aa.io', password='password', bio='Rubber-bodied pirate on an adventurous quest for freedom.', name="Monkey", profile_pic="https://steemitimages.com/1280x0/http://i50.photobucket.com/albums/f331/venitaaresa/luffy2_zpsytpwrang.jpg")
+    tanjiro = User(
+        username='TanjiroKamado', email='tanjiro @aa.io', password='password', bio='Determined demon slayer fueled by love and resilience.', name="Tanjiro", profile_pic="https://pm1.aminoapps.com/7339/ab1f98b1ad4d365807a5a90d9a6d3cd3f80ad54cr1-736-736v2_hq.jpg")
+    nezuko = User(
+        username='NezukoKamado', email='nezuko@aa.io', password='password', bio='Silent demon-turned-protector with fierce loyalty.', name="Nezuko", profile_pic="https://wallpapersmug.com/large/f4f82c/angry-kamado-nezuko-anime.jpg")
+    naruto = User(
+        username='NarutoUzumaki', email='naruto@aa.io', password='password', bio='Energetic ninja dreaming of becoming the Hokage.', name="Naruto", profile_pic="https://w0.peakpx.com/wallpaper/638/468/HD-wallpaper-naruto-live-naruto-uzumaki-portrait-manga.jpg")
+    satoru = User(
+        username='SatoruGojo', email='satoru@aa.io', password='password', bio='Enigmatic sorcerer with unparalleled skill and charisma.', name="Satoru", profile_pic="https://media.distractify.com/brand-img/K3YMRN7w7/1280x670/why-does-gojo-wear-a-mask-1621289501369.png")
+    yuji = User(
+        username='YujiItadori', email='yuji@aa.io', password='password', bio='Fearless exorcist with a compassionate heart.', name="Yuji", profile_pic="https://cdn.pixabay.com/photo/2021/03/14/12/19/sukuna-6094183_1280.jpg")
+    izuku = User(
+        username='IzukuMidoriya', email='izuku@aa.io', password='password', bio='Quirkless hero-in-training striving to save others.', name="Izuku", profile_pic="https://64.media.tumblr.com/316659501dc6f0db36ca0df55c16b9e9/5da25045d004f90c-8c/s1280x1920/df732aab72ff19f02a0fb1e70e19b5d7bbea7a27.jpg")
+    toshinori = User(
+        username='ToshinoriYagi', email='toshinori@aa.io', password='password', bio='Mighty symbol of peace hiding a heroic secret.', name="All Might", profile_pic="https://freeaddon.com/wp-content/uploads/2018/06/all-might-2.jpg")
+    goku = User(
+        username='Goku', email='goku@aa.io', password='password', bio='Legendary Saiyan warrior constantly seeking new challenges.', name="Goku", profile_pic="https://www.skinit.com/media/catalog/product/cache/9dbe6a0c16a5b581719a1aa389879cfc/d/r/drgnbz15_50.jpg")
 
-    demo.followers.append(marnie)
-    demo.followers.append(bobbie)
-    marnie.followers.append(demo)
-    marnie.followers.append(bobbie)
-    bobbie.followers.append(demo)
-    bobbie.followers.append(marnie)
+    demo.followers.append(sasuke)
+    demo.followers.append(monkey)
+    demo.followers.append(tanjiro)
+    demo.followers.append(naruto)
+    demo.followers.append(yuji)
+    demo.followers.append(toshinori)
+    demo.followers.append(goku)
+    demo.followers.append(nezuko)
+
+
+    sasuke.followers.append(demo)
+    sasuke.followers.append(sasuke)
+    sasuke.followers.append(nezuko)
+    sasuke.followers.append(naruto)
+    sasuke.followers.append(satoru)
+    sasuke.followers.append(izuku)
+    sasuke.followers.append(toshinori)
+    sasuke.followers.append(goku)
+
+    sakura.followers.append(demo)
+    sakura.followers.append(sasuke)
+    sakura.followers.append(monkey)
+    sakura.followers.append(tanjiro)
+    sakura.followers.append(nezuko)
+    sakura.followers.append(naruto)
+    sakura.followers.append(yuji)
+    sakura.followers.append(izuku)
+
+    monkey.followers.append(sasuke)
+    monkey.followers.append(monkey)
+    monkey.followers.append(nezuko)
+    monkey.followers.append(naruto)
+    monkey.followers.append(satoru)
+    monkey.followers.append(yuji)
+
+    tanjiro.followers.append(monkey)
+    tanjiro.followers.append(tanjiro)
+    tanjiro.followers.append(nezuko)
+
+    nezuko.followers.append(monkey)
+    nezuko.followers.append(tanjiro)
+    nezuko.followers.append(naruto)
+    nezuko.followers.append(yuji)
+    nezuko.followers.append(izuku)
+
+    naruto.followers.append(tanjiro)
+    naruto.followers.append(nezuko)
+    naruto.followers.append(naruto)
+    naruto.followers.append(satoru)
+
+    satoru.followers.append(monkey)
+    satoru.followers.append(tanjiro)
+    satoru.followers.append(nezuko)
+    satoru.followers.append(naruto)
+    satoru.followers.append(izuku)
+
+    yuji.followers.append(tanjiro)
+    yuji.followers.append(naruto)
+    yuji.followers.append(nezuko)
+    yuji.followers.append(satoru)
+    yuji.followers.append(yuji)
+    yuji.followers.append(izuku)
+    yuji.followers.append(toshinori)
+
+    izuku.followers.append(naruto)
+    izuku.followers.append(satoru)
+    izuku.followers.append(izuku)
+    izuku.followers.append(toshinori)
+    izuku.followers.append(goku)
+
+    toshinori.followers.append(nezuko)
+    toshinori.followers.append(naruto)
+    toshinori.followers.append(satoru)
+    toshinori.followers.append(yuji)
+    toshinori.followers.append(izuku)
+    toshinori.followers.append(toshinori)
+    toshinori.followers.append(goku)
+    toshinori.followers.append(demo)
+    toshinori.followers.append(sakura)
+
+    goku.followers.append(nezuko)
+    goku.followers.append(naruto)
+    goku.followers.append(satoru)
+    goku.followers.append(yuji)
+    goku.followers.append(izuku)
+    goku.followers.append(toshinori)
+    goku.followers.append(goku)
+    goku.followers.append(sakura)
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(sasuke)
+    db.session.add(sakura)
+    db.session.add(monkey)
+    db.session.add(tanjiro)
+    db.session.add(nezuko)
+    db.session.add(naruto)
+    db.session.add(satoru)
+    db.session.add(yuji)
+    db.session.add(izuku)
+    db.session.add(toshinori)
+    db.session.add(goku)
     db.session.commit()
 
 
