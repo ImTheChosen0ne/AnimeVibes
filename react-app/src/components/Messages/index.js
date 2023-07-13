fac15f72894ec37eb86975dfc6ee677c06e647ab
matthew <matthew.almeida@sbcglobal.net>
2023-07-13 13:41:06 -0400

    messaging complete

--- react-app/src/components/Messages/index.js
+++ react-app/src/components/Messages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { io } from "socket.io-client";
 import { fetchUsers } from "../../store/user";
@@ -17,6 +17,7 @@ function Message() {
   const [activeChatId, setActiveChatId] = useState(null);
   const sessionUser = useSelector((state) => state.session.user);
   const users = Object.values(useSelector((state) => state.users));
+  const sentMessagesRef = useRef(null);
 
   useEffect(() => {
     dispatch(fetchUsers());
@@ -41,6 +42,12 @@ function Message() {
       socket.disconnect();
     };
   }, []);
+
+  useEffect(() => {
+    if (sentMessagesRef.current) {
+      sentMessagesRef.current.scrollTop = sentMessagesRef.current.scrollHeight;
+    }
+  }, [activeChatId, messages]);
 
   const updateChatInput = (e) => {
     setChatInput(e.target.value);
@@ -82,7 +89,7 @@ function Message() {
             <h1>Messages</h1>
             <OpenModalButton
               buttonText="New Chat"
-              modalComponent={<NewChat />}
+              modalComponent={<NewChat setActiveChatId={setActiveChatId}/>}
             />
           </div>
           <div className="all-chats">
@@ -118,6 +125,7 @@ function Message() {
                               <DeleteChat
                                 userId={sessionUser.id}
                                 chatId={chat.id}
+                                setActiveChat={setActiveChatId}
                               />
                             }
                           />
@@ -131,64 +139,101 @@ function Message() {
           </div>
         </div>
         <div className="messages">
-        <form onSubmit={sendChat}>
-            <input value={chatInput} onChange={updateChatInput} />
-            <button type="submit">Send</button>
-          </form>
-              <div>
+          {activeChatId && (
+            <div className="selected-chat">
+              <div className="selected-chat-title">
+                <img
+                  src={getChatUsername(activeChatId)?.profile_pic}
+                  alt="user"
+                ></img>
+                <h2>{getChatUsername(activeChatId)?.username}</h2>
+              </div>
+              <div className="sent-messages" ref={sentMessagesRef}>
+                {users
+                  .map((user) => user.messages)
+                  .flat()
+                  .filter((message) => message?.chatId === activeChatId)
+                  .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+                  .map((message) => (
+                    <div
+                      key={message.id}
+                      className={`message ${
+                        sessionUser.id === message.userId ? "own-message" : ""
+                      }`}
+                    >
+                      <div
+                        className={`message-info ${
+                          sessionUser.id === message.userId ? "own-message" : ""
+                        }`}
+                      >
+                        <img src={message.profile_pic} alt="user"></img>
+                        <p>{message.message}</p>
+                      </div>
+
+                      {sessionUser.id === message.userId && (
+                        <div className="name-ellipse-message">
+                          <div className="ellipse-pulldown-message">
+                            <div className="main-button-message">⋯</div>
+                            <div className="dropdown-content-message">
+                              <div className="message-delete-button">
+                                <button
+                                  onClick={() =>
+                                    deleteMessages(message.id, message.userId)
+                                  }
+                                >
+                                  Delete
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  ))}
                 {messages
                   .filter((message) => message.chatId === activeChatId)
                   .map((message, ind) => (
-                    <div key={ind}>
-                      <img src={message.profile_pic} alt="user"></img>
-                      {message.message}
+                    <div
+                      key={ind}
+                      className={`message-end ${
+                        sessionUser.id === message.userId ? "own-message" : ""
+                      }`}
+                    >
+                      <div className="message-end-info">
+                        <img src={message.profile_pic} alt="user"></img>
+                        <p>{message.message}</p>
+                      </div>
                       {sessionUser.id === message.userId && (
-                        <button onClick={() => deleteMessage(message.id)}>
-                          Delete
-                        </button>
+                        <div className="name-ellipse-message">
+                          <div className="ellipse-pulldown-message">
+                            <div className="main-button-message">⋯</div>
+                            <div className="dropdown-content-message">
+                              <div className="message-delete-button">
+                                <button
+                                  onClick={() => deleteMessage(message.id)}
+                                >
+                                  Delete
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                       )}
                     </div>
                   ))}
               </div>
-          <div>
-            {activeChatId && (
-              <div className="selected-chat">
-                {activeChatId && (
-                  <div className="selected-chat">
-                    <img
-                      src={getChatUsername(activeChatId)?.profile_pic}
-                      alt="user"
-                    ></img>
-                    <h2>{getChatUsername(activeChatId)?.username}</h2>
-                    {users
-                      .map((user) => user.messages)
-                      .flat()
-                      .filter((message) => message?.chatId === activeChatId)
-                      .sort(
-                        (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
-                      )
-                      .map((message) => (
-                        <div key={message.id}>
-                          <div>
-                            <img src={message.profile_pic} alt="user"></img>
-                            {message.message}
-                          </div>
-                          {sessionUser.id === message.userId && (
-                            <button
-                              onClick={() =>
-                                deleteMessages(message.id, message.userId)
-                              }
-                            >
-                              Delete
-                            </button>
-                          )}
-                        </div>
-                      ))}
-                  </div>
-                )}
+              <div className="messages-input-container">
+                <form onSubmit={sendChat}>
+                  <input
+                    value={chatInput}
+                    onChange={updateChatInput}
+                    placeholder="Send a message..."
+                  />
+                  <button type="submit">Send</button>
+                </form>
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
       </div>
     )
